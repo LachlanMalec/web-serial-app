7c7a6606e154d2e80f18e366aaf115030b05fc99
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-11-26 16:39:22 +1000

    feat: send data to server for persistence

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use axum::{response::Html, routing::get, Router, Server};
+use axum::{
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+    Form, Router, Server,
+};
 use std::net::SocketAddr;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(serve_html));
+    let app = Router::new()
+        .route("/", get(serve_html))
+        .route("/write", post(write_data));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on http://{}", addr);
@@ -15,6 +24,12 @@ async fn main() {
 
 async fn serve_html() -> Html<&'static str> {
     Html(include_str!("index.html"))
+}
+
+async fn write_data(Form(input): Form<Input>) -> StatusCode {
+    let mut file = File::create("data.txt").await.unwrap();
+    file.write_all(input.data.as_bytes()).await.unwrap();
+    StatusCode::OK
 }
 
 #[derive(serde::Deserialize)]
