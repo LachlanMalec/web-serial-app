5f54797f7d61d7aa26656d04c4650726997ba455
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-11-26 16:12:54 +1000

    feat: simple axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router, Server};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(serve_html));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on http://{}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn serve_html() -> Html<&'static str> {
+    Html("<!DOCTYPE html><html><body><h1>Hello, world!</h1></body></html>")
 }
